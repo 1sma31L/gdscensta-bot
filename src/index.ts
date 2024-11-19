3bb21dfda63560360ffee6156d4cb9f4c1b9a9ed
ismail <ismailismo78@gmail.com>
2024-11-19 15:35:22 +0100

    refactor: enhance server initialization in index.ts with improved structure and comments

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,26 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+// Loading enviorement variables
 dotenv.config();
 
+// Initializing the server
 const app = express();
 const PORT = process.env.PORT || "3000";
 
+// Starting the server
+app.listen(PORT, () => {
+    console.log(`Server is running : http://localhost:${PORT}`);
+});
+
+// Using Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Defining Endpoints and Routes
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
     res.status(200);
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running : http://localhost:${PORT}`);
 });
 
 export default app;
