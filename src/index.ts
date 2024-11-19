fef6dbeb7a568441d732edd56c2265a30d33761b
ismail <ismailismo78@gmail.com>
2024-11-19 15:26:46 +0100

    refactor: update Prettier configuration, remove ESLint config, and adjust TypeScript settings

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-  res.status(200);
+    res.send('Hello World');
+    res.status(200);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running : http://localhost:${PORT}`);
+    console.log(`Server is running : http://localhost:${PORT}`);
 });
 
 export default app;
