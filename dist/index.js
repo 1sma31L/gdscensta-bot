81618597e46fa7bd30d98c03ed2e68d52601635b
ismail <ismailismo78@gmail.com>
2024-11-27 00:52:46 +0100

    feat: implement getChatCompletion function and remove old controller file

--- dist/index.js
+++ dist/index.js
@@ -11,10 +11,41 @@ import TelegramBot from "node-telegram-bot-api";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
-import { getChatCompletion } from "./controllers/Chatbot";
 dotenv.config();
 const BOT_TOKEN = process.env.TELEGRAM_BOT_API;
 const WEBHOOK_URL = "https://gdscensta-bot.vercel.app";
+const getChatCompletion = (text) => __awaiter(void 0, void 0, void 0, function* () {
+    const url = "https://api.x.ai/v1/chat/completions";
+    const apiKey = process.env.GROK_API_KEY;
+    const payload = {
+        messages: [
+            {
+                role: "system",
+                content: "You are a telegram assistant of the club gdsc ensta national higher school of adnced technologies we have 5 dapertments web dev app dev cyber sec and ai and cultural.",
+            },
+            {
+                role: "user",
+                content: text,
+            },
+        ],
+        model: "grok-beta",
+        stream: false,
+        temperature: 0,
+        max_tokens: 60,
+    };
+    const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+    };
+    const response = yield fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(payload),
+    });
+    const data = yield response.json();
+    return data;
+});
+//
 const app = express();
 app.use(bodyParser.json());
 const bot = new TelegramBot(BOT_TOKEN);
