0d5bc9f44cd249f19b640fc3c563cfb7187a573b
ismail <ismailismo78@gmail.com>
2024-11-26 23:30:48 +0100

    feat: remove Telegram bot functionality and associated department data

--- dist/data/index.js
+++
@@ -1,5 +0,0 @@
-export const departments = {
-    tech: ["Frontend Group", "Backend Group"],
-    design: ["UI/UX Group", "Graphic Design Group"],
-    marketing: ["Content Creation", "Social Media"],
-};
