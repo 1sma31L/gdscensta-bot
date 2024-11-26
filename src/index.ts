ae3c87850e040c08327f5705e9eadc1167dd3682
ismail <ismailismo78@gmail.com>
2024-11-26 23:30:50 +0100

    ww

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,32 @@ import dotenv from "dotenv";
 import express from "express";
 
 dotenv.config();
-// Replace with your bot's token
 const BOT_TOKEN = process.env.TELEGRAM_BOT_API!;
-const WEBHOOK_URL = "https://gdscensta-bot.vercel.app"; // Replace with your webhook URL
+const WEBHOOK_URL = "https://gdscensta-bot.vercel.app";
 
 const app = express();
 app.use(bodyParser.json());
 
-// Initialize the bot in webhook mode
 const bot = new TelegramBot(BOT_TOKEN);
 bot.setWebHook(WEBHOOK_URL);
 
-// Define the bot logic
 bot.on("message", (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
     if (text === "/start") {
         bot.sendMessage(chatId, "Welcome to the bot! How can I assist you?");
+        bot.getMyCommands();
     } else {
         bot.sendMessage(chatId, `You said: ${text}`);
     }
 });
 
-// Handle incoming webhook requests
 app.post("/", (req, res) => {
     bot.processUpdate(req.body);
     res.sendStatus(200);
 });
 
-// Start the Express server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
