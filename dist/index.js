a6a282e9a9a1c20f3a98a14f6ee0fe60d2796d9e
ismail <ismailismo78@gmail.com>
2024-11-26 21:08:18 +0100

    fix: update .gitignore to remove dist/ directory and clean up package.json scripts

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+// Loading enviorement variables
+dotenv.config();
+// Initializing the server
+const app = express();
+const PORT = "3000";
+// Using Middlewares
+app.use(express.json());
+// Defining Endpoints and Routes
+app.get("/", (req, res) => {
+    res.json("Hello World!");
+    res.status(200);
+});
+app.post("/", (req, res) => {
+    res.json("Hello World!");
+    res.status(200);
+});
+// Starting the server
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT:${PORT}`);
+});
+export default app;
