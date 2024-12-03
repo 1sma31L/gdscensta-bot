6039e3759d95a4250b322002310acc222c610b59
ismail <ismailismo78@gmail.com>
2024-12-03 15:48:35 +0100

    feat: add commands for listing departments and sub-departments in bot interaction

--- src/index.ts
+++ src/index.ts
@@ -25,12 +25,13 @@ bot.on("message", (msg) => {
 		"Cyber Security": "https://t.me/cybersec_group",
 		"UI/UX Design": "https://t.me/uiux_group",
 		"Human Resources": "https://t.me/hr_group",
-		"External Relations": "https://t.me/er_group",
+		"External Relations": "https://t.me/+jeqsgIzFIJZlODQ0",
+		Projects: "https://t.me/+SHCfGII7Y3AzMjY0",
 		Design: "https://t.me/design_group",
 		IT: "https://t.me/it_group",
 	};
 
-	// Check for the `/start` command
+	// Commands
 	if (text === "/start") {
 		bot.sendMessage(chatId, "Welcome to our club! Choose a department:", {
 			reply_markup: {
@@ -47,7 +48,37 @@ bot.on("message", (msg) => {
 		});
 		return;
 	}
+	if (text === "/departments") {
+		bot.sendMessage(
+			chatId,
+			"Links to all of our departments:\n\n" +
+				`Web Development: ${DEP_LINKS["Web Development"]}\n` +
+				`App Development: ${DEP_LINKS["App Development"]}\n` +
+				`Artificial Intelligence: ${DEP_LINKS["Artificial Intelligence"]}\n` +
+				`Cyber Security: ${DEP_LINKS["Cyber Security"]}\n` +
+				`UI/UX Design: ${DEP_LINKS["UI/UX Design"]}\n` +
+				`Human Resources: ${DEP_LINKS["Human Resources"]}\n` +
+				`External Relations: ${DEP_LINKS["External Relations"]}\n` +
+				`Design: ${DEP_LINKS["Design"]}\n` +
+				`IT: ${DEP_LINKS["IT"]}\n` +
+				`Projects: ${DEP_LINKS["Projects"]}`
+		);
+		return;
+	}
+	if (text === "/sub-departments") {
+		bot.sendMessage(
+			chatId,
+			"Links to all of our sub-departments:\n\n" +
+				`Human Resources: ${DEP_LINKS["Human Resources"]}\n` +
+				`External Relations: ${DEP_LINKS["External Relations"]}\n` +
+				`Design: ${DEP_LINKS["Design"]}\n` +
+				`IT: ${DEP_LINKS["IT"]}\n` +
+				`Projects: ${DEP_LINKS["Projects"]}`
+		);
+		return;
+	}
 
+	// Department
 	switch (text) {
 		case "Web Development":
 		case "App Development":
@@ -56,8 +87,7 @@ bot.on("message", (msg) => {
 		case "UI/UX Design":
 			bot.sendMessage(
 				chatId,
-				`You chose ${text}. Here's the link to the Recruitment team Telegram group: ${DEP_LINKS[text]}`,
-				{}
+				`You chose ${text}. Here's the link to the Recruitment team Telegram group: ${DEP_LINKS[text]}`
 			);
 			bot.sendMessage(chatId, "Choose a Sub-Department", {
 				reply_markup: {
@@ -66,6 +96,7 @@ bot.on("message", (msg) => {
 						[{ text: "External Relations" }],
 						[{ text: "Design" }],
 						[{ text: "IT" }],
+						[{ text: "Projects" }],
 					],
 					resize_keyboard: true,
 					one_time_keyboard: true,
@@ -78,21 +109,15 @@ bot.on("message", (msg) => {
 					case "External Relations":
 					case "Design":
 					case "IT":
+					case "Projects":
 						bot.sendMessage(
 							chatId,
 							`You chose ${text}. Here's the link to the ${text} Telegram group: ${DEP_LINKS[text]}`
 						);
 						break;
-					default:
-						bot.sendMessage(
-							chatId,
-							"Please choose a valid option from the menu."
-						);
 				}
 			});
 			break;
-		default:
-			bot.sendMessage(chatId, "Please choose a valid option from the menu.");
 	}
 });
 
