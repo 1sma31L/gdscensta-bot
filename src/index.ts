7a5e337e59b01ce1d93d43e77b5e99a21c1e6ddd
ismail <ismailismo78@gmail.com>
2024-12-03 21:12:40 +0100

    feat: update department links in bot interaction for improved accessibility

--- src/index.ts
+++ src/index.ts
@@ -21,17 +21,17 @@ bot.on("message", (msg) => {
 	const text = msg.text;
 	if (!text) return;
 	const DEP_LINKS: { [key: string]: string } = {
-		"Web Development": "https://t.me/webdev_group",
-		"App Development": "https://t.me/appdev_group",
-		"Artificial Intelligence": "https://t.me/ai_group",
-		"Cyber Security": "https://t.me/cybersec_group",
-		"UI/UX Design": "https://t.me/uiux_group",
-		"Human Resources": "https://t.me/hr_group",
+		"Web Development": "https://t.me/+_IPmZ5-TpF1mOTI0",
+		"App Development": "https://t.me/+88L0_qp2s3M1NjY0",
+		"Artificial Intelligence": "https://t.me/+AdP_VzxMQ6A0OGI0",
+		"Cyber Security": "https://t.me/+amZMY17AL2A3NjFk",
+		"UI/UX Design": "https://t.me/+GrYRScDqKl4yZTM0",
+		"Human Resources": "https://t.me/+kEIV-6t-qbo2YTFk",
 		"External Relations": "https://t.me/+jeqsgIzFIJZlODQ0",
 		Projects: "https://t.me/+SHCfGII7Y3AzMjY0",
-		Design: "https://t.me/design_group",
-		IT: "https://t.me/it_group",
-		MAIN: "https://t.me/gdscensta",
+		Communications: "https://t.me/+z2KLzu3ZQxllZDA0",
+		IT: "https://t.me/+neKBXQqCmf44MWE8",
+		MAIN: "https://t.me/+EKvjqHIlk9RjNjk0",
 	};
 
 	// Commands
@@ -76,7 +76,7 @@ bot.on("message", (msg) => {
 			"Links to all of our sub-departments:\n\n" +
 				`Human Resources: ${DEP_LINKS["Human Resources"]}\n` +
 				`External Relations: ${DEP_LINKS["External Relations"]}\n` +
-				`Design: ${DEP_LINKS["Design"]}\n` +
+				`Communications: ${DEP_LINKS["Communications"]}\n` +
 				`IT: ${DEP_LINKS["IT"]}\n` +
 				`Projects: ${DEP_LINKS["Projects"]}\n`
 		);
